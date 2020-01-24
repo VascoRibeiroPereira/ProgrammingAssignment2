62553c3ed49d21636cfbd9027d01b57a53c6f731
Vasco Alberto Filipe Ribeiro <vascoalbertofiliperibeiro@MBP-de-Vasco.home>
2020-01-24 23:53:54 +0000

    Added extensive explanation of how this code works for the assignment and also for furure reference.

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The cachematrix.R file contains two functions, makeCacheMatrix() and 
+## cacheSolve(). The first function in the file creates an R object 
+## that stores a matrix and its inverse. 
+## The second function requires an argument that is returned by 
+## makeCacheMatrix() in order to retrieve the inverse from the 
+## cached matrix that is stored in the makeCacheMatrix() object's 
+## environment.
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## set the value of x and NULL to the value of i - so whenever x is reset the value of 
+## i cached in the memory of the object is cleared, forcing subsequent 
+## calls to cacheSolve() to recalculate the inverse rather than retrieving 
+## the wrong value from cache.
+
+## getter for the matrix (x) defined
+## setter for the inverse (i) defined
+
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
+        
         set <- function(y) {
                 x <<- y
                 i <<- NULL
         }
+
         get <- function() x
-        setinverse <- function(solve) i <<- solve
+        setinverse <- function(solve) i <<- solve ## the <<- assignment operator to assign the input argument to the value of i in the parent environment
         getinverse <- function() i
-        list(set = set, get = get,
-             setinverse = setinverse,
-             getinverse = getinverse)
+        
+        list(set = set,                ## gives the name 'set' to the set() function defined above
+             get = get,                ## gives the name 'get' to the get() function defined above
+             setinverse = setinverse,  ## gives the name 'setinverse' to the setinverse() function defined above
+             getinverse = getinverse)  ## gives the name 'getinverse' to the getinverse() function defined above
         
         
 }
 
-
-## Write a short comment describing this function
+## cacheSolve
+## cacheSolve() is the only place where the solve() function is executed
+## the makeCacheMatrix would be incomplete without cacheSolve()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
-        i <- x$getinverse()
+        i <- x$getinverse() ##calling getinverse() on the input object
         if(!is.null(i)) {
                 message("getting cached data")
-                return(i)
+                return(i) 
+                
+## if not NULL cached i returned to parent environment
+## if !is.null(i) is FALSE, cachemean() gets the matrix from the input object, 
+## calculates a solve(), uses the setinverse() function on the input object to 
+## set the inverse in the input object, and then returns the value of the inverse 
+## to the parent environment by printing the inverted object.
+                
         }
         data <- x$get()
         i <- solve(data, ...)
-        x$setinverse(i)
+        x$setinverse(i) ## set the inverse in the input object, and then returns the value of the inverse to the parent environment by printing the inverted object.
         i
 }
